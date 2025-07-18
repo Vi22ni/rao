1024c69f9cdd43e807e7f31f2c7f41bdaf8294f3
Vi22ni <vinicius22bueno@gmail.com>
2025-07-18 12:05:17 -0300

    feat: vuerouter setup

--- apps/frontend/vue-project/env.d.ts
+++ apps/frontend/vue-project/env.d.ts
@@ -1,8 +1,15 @@
 interface ImportMetaEnv {
   readonly VITE_API_URL: string
   readonly VITE_MAP_KEY?: string
+  readonly BASE_URL: string
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv
+}
+
+declare module '*.vue' {
+  import { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
 }
--- apps/frontend/vue-project/src/main.ts
+++ apps/frontend/vue-project/src/main.ts
@@ -6,10 +6,12 @@ import PrimeVue from 'primevue/config'
 // import 'primevue/resources/themes/lara-light-teal/theme.css'
 // import 'primeflex/primeflex.css'
 import App from './App.vue'
+import router from './router';
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(PrimeVue)
+app.use(router);
 
 app.mount('#app')
